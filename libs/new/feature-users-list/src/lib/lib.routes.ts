4106f008a3c31a97297080f36c8cc70d1f2394ee
NursultanFront <nursultan020395@gmail.com>
2023-12-05 23:18:25 +0500

    feat: delete useless library

--- libs/new/feature-users-list/src/lib/lib.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Route } from '@angular/router';
-import { FeatureUsersListComponent } from './feature-users-list/feature-users-list.component';
-
-export const featureUsersListRoutes: Route[] = [
-  { path: '', component: FeatureUsersListComponent },
-];
